8ab2757d69d3b22af90732b70275fea88805ddfe
shaybay <pacificconnect310@gmail.com>
2023-01-15 12:44:00 -0800

    complete slot machine lines and rows and logic

--- main.py
+++ main.py
@@ -13,6 +13,52 @@ symbol_count = {
     "C":6,
     "D":8
 }
+symbol_value = {
+    "A":5,
+    "B":4,
+    "C":3,
+    "D":2
+}
+
+def check_winning(columns, lines, bet, values):
+    winnings = 0
+    winning_lines = [] 
+    for line in range(lines):
+        symbol = columns [0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+        else:
+            winnings += values[symbol] * bet
+            winning_lines.append(line + 1)
+    return winnings, winning_lines
+
+def get_slot_machine_spin(rows, cols,symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+    columns = []
+    for _ in range(cols):
+        column = [] 
+        current_symbols = all_symbols[:]
+        for row in range(rows):
+                value = random.choice(current_symbols)
+                current_symbols.remove(value)
+                column.append(value)    
+        columns.append(column )   
+    return columns
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i,  column in enumerate(columns):
+            if i != len(columns) -1:
+                print(column[row], end=" | ")
+            else:
+                print (column[row], end="")
+ 
+        print()
 
 def deposit():
     while True:
@@ -66,6 +112,11 @@ def main():
     bet = get_bet() 
     total_bet = bet * lines
     print(f"You are betting ${bet} on ${lines} lines. Total bet is ${total_bet}")
-  
+
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
+    winnings, winning_lines = check_winning(slots, lines, bet, symbol_value)
+    print(f"You won {winnings}.")
+    print(f"on lines: ", *winning_lines)
 
 main()
